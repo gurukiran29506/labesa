5ef0612c71d2859aafe88e02397aef44492c907f
gurukiran29506 <gurukiranhippargi@gmail.com>
2026-01-17 09:12:06 +0530

    Update test_car.py

--- test_car.py
+++ test_car.py
@@ -1,9 +1,9 @@
 from car import car_details
 def test_car_details():
-  expected_output = {
+  expected output = {
     "name=BMW",
     "model=M5",
     "price=25000000",
     "color=black"
    }
-assert car_details("BMW", "M5", 25000000, "black") == expected_output
+assert car_details("BMW", "M5", 25000000, "black") == expected output
