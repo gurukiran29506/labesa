443cf96ceac938c5669f53360f04115a0112fb2f
gurukiran29506 <gurukiranhippargi@gmail.com>
2026-01-17 09:27:11 +0530

    Fix variable name in car details test

--- test_car.py
+++ test_car.py
@@ -1,14 +1,10 @@
 from car import car_details
 
 def test_car_details():
-    name = "BMW"
-    model = "M5"
-    price = 25000000
-    color = "Black"
-    expected_output = (
+    expected_result = (
         "name=BMW",
         "model=M5",
         "price=25000000",
         "color=Black"
     )
-    assert car_details(name, model, price, color) == expected_output
+    assert car_details(name, model, price, color) == expected_result
