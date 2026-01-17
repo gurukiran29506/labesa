e993c330bda35c5418ab2a95d3352f83ec218009
gurukiran29506 <gurukiranhippargi@gmail.com>
2026-01-17 09:22:45 +0530

    Fix syntax error in test_car.py

--- test_car.py
+++ test_car.py
@@ -2,10 +2,10 @@ from car import car_details
 
 
 def test_car_details():
-  expected_result == {
+  expected_result = (
     "name=BMW",
     "model=M5",
     "price=25000000",
     "color=black"
-   }
+  )
 assert car_details("BMW", "M5", 25000000, "black") == expected_result
